d67d34ee7e085683f1104cbb369dd3896a7fcde2
blackoutdc <blackoutdc@bigmir.net>
2020-11-01 18:30:09 +0200

    Добавил пакет AForge DirectShow в проэкт Producer

--- Producer/Program.cs
+++ Producer/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +10,14 @@ namespace Producer
 {
     class Program
     {
+        private static IPEndPoint consumerEndPoint;
+
         static void Main(string[] args)
         {
+            var consumerIP = ConfigurationManager.AppSettings.Get("consumerIP");
+            var consumerPort = int.Parse(ConfigurationManager.AppSettings.Get("consumerPort"));
+            consumerEndPoint = new IPEndPoint(IPAddress.Parse(consumerIP), consumerPort);
+            Console.WriteLine($"consumer: {consumerEndPoint}");
         }
     }
 }
