da8537e17f3cb8c53d3a5d72e0b30f6644362119
Ognelad-B <70412189+Ognelad-B@users.noreply.github.com>
2020-11-16 21:59:09 +0200

    Создал новую ветвь))

--- Consumer/Form1.Designer.cs
+++ Consumer/Form1.Designer.cs
@@ -50,6 +50,7 @@
             this.ClientSize = new System.Drawing.Size(1008, 729);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
